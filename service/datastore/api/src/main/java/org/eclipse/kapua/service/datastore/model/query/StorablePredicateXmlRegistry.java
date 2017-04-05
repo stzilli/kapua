e3759ccdb9f1e28bf3f194789d2d5b09b00c15c1
coduz <alberto.codutti@eurotech.com>
2017-04-05 12:11:50 +0200

    Applied code formatting and import cleanup
    Signed-off-by: coduz <alberto.codutti@eurotech.com>

--- service/datastore/api/src/main/java/org/eclipse/kapua/service/datastore/model/query/StorablePredicateXmlRegistry.java
+++ service/datastore/api/src/main/java/org/eclipse/kapua/service/datastore/model/query/StorablePredicateXmlRegistry.java
@@ -3,21 +3,21 @@ package org.eclipse.kapua.service.datastore.model.query;
 import org.eclipse.kapua.locator.KapuaLocator;
 
 public class StorablePredicateXmlRegistry {
-    
+
     private static final StorablePredicateFactory factory = KapuaLocator.getInstance().getFactory(StorablePredicateFactory.class);
-    
+
     public TermPredicate<?> newTermPredicate() {
         return factory.newTermPredicate(null, null);
     }
-    
+
     public AndPredicate newAndPredicate() {
         return factory.newAndPredicate();
     }
-    
+
     public ChannelPredicate newChannelPredicate() {
         return factory.newChannelPredicate(null);
     }
-    
+
     public ExistsPredicate newExistsPredicate() {
         return factory.newExistsPredicate(null);
     }
